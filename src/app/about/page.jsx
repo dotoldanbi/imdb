25a0c3b6cb92b2972b6963a96fabffaf2d99fc85
jhsong <jh@song>
2025-03-20 21:33:25 +0900

    about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div className="max-w-6xl mx-auto p-3 gap-4 space-y-4">
+      <h1 className="text-2xl font-medium text-amber-600">About</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sagittis
+        purus eu interdum ultrices. Sed nec metus mauris. Nullam eros mi, mollis
+        nec viverra eget, pellentesque in nunc. Maecenas sit amet bibendum
+        libero, eget laoreet est. Sed pellentesque sagittis suscipit. Cras
+        eleifend feugiat leo, id finibus urna pretium sit amet. Vestibulum a
+        pellentesque nunc, et tristique justo. Pellentesque mattis sed enim eu
+        placerat. Suspendisse pulvinar pulvinar augue, ac feugiat erat. Praesent
+        vitae auctor urna.
+      </p>
+      <p>
+        Nullam placerat tempor lectus a luctus. Aliquam erat volutpat. Cras quis
+        dictum augue, a mattis leo. Nulla efficitur blandit nisl, in blandit
+        lorem fermentum vel. Nulla facilisi. Fusce fermentum ornare mi vel
+        mattis. Quisque vehicula lorem et risus tempor, commodo fermentum nibh
+        tincidunt. Aenean at libero et nisi suscipit aliquet. Aenean hendrerit
+        enim vel leo viverra placerat consequat at sem. Duis ipsum odio,
+        facilisis ac consequat quis, viverra ac metus. Proin quam felis,
+        vehicula in tincidunt a, congue a tortor. In ut turpis in nisi eleifend
+        cursus.
+      </p>
+      <p>
+        Vivamus fringilla odio non nisi cursus consectetur. Nullam sed mauris
+        nec felis interdum pellentesque sed eget nibh. Fusce convallis pulvinar
+        nulla, a vehicula ipsum dignissim tempor. Curabitur et fringilla enim.
+        Suspendisse potenti. Ut dignissim, risus non consequat gravida, neque
+        odio facilisis nisl, id tempus nisi enim vitae erat. Aliquam nunc odio,
+        pharetra mollis varius rutrum, congue in elit. Mauris ut mattis sapien,
+        vitae tincidunt lectus. Morbi finibus magna vel pharetra maximus.
+        Praesent et fringilla quam. Proin sit amet auctor mauris. Nunc vel augue
+        quis augue scelerisque sollicitudin.
+      </p>
+    </div>
+  );
+}
