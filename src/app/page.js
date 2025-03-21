60ec7768963e5c7962ce22265a81bb609f966f7a
jhsong <jh@song>
2025-03-21 17:56:56 +0900

    loading effect

--- src/app/page.js
+++ src/app/page.js
@@ -7,11 +7,19 @@ export default async function Home({ searchParams }) {
   console.log(API_KEY);
   // const genre = searchParams.genre || "fetchTrending";
   const genre = "fetchTrending";
-  const res = await fetch(
-    `https://api.themoviedb.org/3/${
-      genre === "fetchTopRated" ? `/movie/top_rated` : `trending/all/week`
-    }?api_key=${API_KEY}&language=en-US&page=1`
-  );
+
+  const res = await new Promise((resolve) => {
+    // setTimeout(async () => {
+    const response = fetch(
+      `https://api.themoviedb.org/3/${
+        genre === "fetchTopRated" ? `/movie/top_rated` : `trending/all/week`
+      }?api_key=${API_KEY}&language=en-US&page=1`,
+      { next: { revalidate: 10000 } }
+    );
+    resolve(response);
+    // }, 2000);
+  });
+
   console.log(res);
   const data = await res.json();
   if (data) {
