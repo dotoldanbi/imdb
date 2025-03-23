3587bdb68cc9d3bee704a2d87e0f139f0d483687
jhsong <jh@song>
2025-03-23 22:13:38 +0900

    fix errors

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import NavBarItem from "./NavBarItem";
-
+import { Suspense } from "react";
 export default function NavBar() {
   return (
-    <div className="flex p-4 justify-center dark:bg-amber-600  bg-amber-100 lg:text-lg justify-center gap-4">
-      <NavBarItem title="Trending" param="fetchTrending" />
-      <NavBarItem title="Top Rated" param="fetchTopRated" />
-    </div>
+    <Suspense>
+      <div className="flex p-4 justify-center dark:bg-amber-600  bg-amber-100 lg:text-lg justify-center gap-4">
+        <NavBarItem title="Trending" param="fetchTrending" />
+        <NavBarItem title="Top Rated" param="fetchTopRated" />
+      </div>
+    </Suspense>
   );
 }
